3b905bb296ff0413c8ac9a69ed7464cb42ef685f
Evan Brumley <evan.brumley@wspdigital.com>
2015-01-06 09:50:59 +0000

    Added player edit button

--- spyfall/client/main.js
+++ spyfall/client/main.js
@@ -320,13 +320,18 @@ Template.lobby.helpers({
   },
   players: function () {
     var game = getCurrentGame();
+    var currentPlayer = getCurrentPlayer();
 
     if (!game) {
       return null;
     }
 
-    var players = Players.find({
-      'gameID': game._id
+    var players = Players.find({'gameID': game._id}).fetch();
+
+    players.forEach(function(player){
+      if (player._id === currentPlayer._id){
+        player.isCurrent = true;
+      }
     });
 
     return players;
@@ -364,6 +369,12 @@ Template.lobby.events({
   'click .btn-remove-player': function (event) {
     var playerID = $(event.currentTarget).data('player-id');
     Players.remove(playerID);
+  },
+  'click .btn-edit-player': function (event) {
+    var game = getCurrentGame();
+    resetUserState();
+    Session.set('urlAccessCode', game.accessCode);
+    Session.set('currentView', 'joinGame');
   }
 });
 
