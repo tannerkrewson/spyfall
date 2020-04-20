227a5bfedb87256e168f6297d0df4fe89f3d1bf1
Tanner Krewson <tannerkrewson@gmail.com>
2020-04-19 23:42:01 -0500

    prevent zoom on input on iOS

--- components/Page.js
+++ components/Page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 import "../styles/sanitize.css";
 import "skeleton-css/css/skeleton.css";
@@ -8,14 +9,23 @@ import LanguageSelector from "./LanguageSelector";
 import Footer from "./Footer";
 
 const Page = ({ children }) => (
-	<div dir="ltr" className="container dir-ltr lang-en">
-		<div className="main-content">
-			{children}
-			<hr />
-			<LanguageSelector />
-			<Footer />
+	<>
+		<Head>
+			<title>Spyfall</title>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1, maximum-scale=1"
+			/>
+		</Head>
+		<div dir="ltr" className="container dir-ltr lang-en">
+			<div className="main-content">
+				{children}
+				<hr />
+				<LanguageSelector />
+				<Footer />
+			</div>
 		</div>
-	</div>
+	</>
 );
 
 export default Page;
