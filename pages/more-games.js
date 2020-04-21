4a7aa09587450d0cd7763014bb567ae5dca0b6fa
Tanner Krewson <tannerkrewson@gmail.com>
2020-04-21 12:40:45 -0500

    update more games

--- pages/more-games.js
+++ pages/more-games.js
@@ -9,71 +9,76 @@ const MoreGames = () => {
 			<div className="main-menu">
 				<h3>Games Like Spyfall</h3>
 				<hr />
-				<div className="game">
-					<a
-						href="https://drawphone.tannerkrewson.com/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Drawphone
-					</a>
-					<div> by Tanner Krewson</div>
-				</div>
-				<div className="game">
-					<a
-						href="https://snakeout.tannerkrewson.com/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Snakeout
-					</a>
-					<div> by Tanner Krewson</div>
-				</div>
-				<div className="game">
-					<a
-						href="https://jackboxgames.com/games/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						The Jackbox Party Packs
-					</a>
-					<div> by Jackbox Games</div>
-				</div>
-				<div className="game">
-					<a
-						href="http://www.secrethitler.party/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Out of Context
-					</a>
-					<div> by Isaac Hirschfeld</div>
-				</div>
-				<div className="game">
-					<a
-						href="http://www.secrethitler.party/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Secret Hilter
-					</a>
-					<div> by Samuel Mak</div>
-				</div>
+				<GameLink
+					title="Drawphone"
+					subtitle="by Tanner Krewson"
+					link="https://drawphone.tannerkrewson.com/"
+				/>
+
+				<GameLink
+					title="The Jackbox Party Packs"
+					subtitle="by Jackbox Games"
+					link="https://jackboxgames.com/games/"
+				/>
+
+				<GameLink
+					title="Codenames"
+					subtitle="by Jackson Owens"
+					link="https://www.horsepaste.com/"
+				/>
+
+				<GameLink
+					title="netgames.io"
+					subtitle="by Luke Tsekouras"
+					featuring="ft. Werewolf, Love Letter, Avalon & more"
+					link="https://netgames.io/"
+				/>
+
+				<GameLink
+					title="Out of Context"
+					subtitle="by Isaac Hirschfeld"
+					link="https://www.outofcontext.party/"
+				/>
+
+				<GameLink
+					title="Secret Hilter"
+					subtitle="by Samuel Mak"
+					link="http://www.secrethitler.party/"
+				/>
+
+				<GameLink
+					title="Snakeout"
+					subtitle="by Tanner Krewson"
+					link="https://snakeout.tannerkrewson.com/"
+				/>
 			</div>
 			<Link href="/">
 				<button>Back to Spyfall</button>
 			</Link>
-			<style jsx>{`
-				.game {
-					margin-bottom: 1.2em;
-					font-size: 0.9em;
-				}
-				a {
-					font-size: 1.3em;
-				}
-			`}</style>
 		</>
 	);
 };
+
+const GameLink = ({ title, link, subtitle, featuring }) => (
+	<div className="game">
+		<a href={link} target="_blank" rel="noopener noreferrer">
+			{title}
+		</a>
+		{featuring && <div>{featuring}</div>}
+		<div>
+			<i>{subtitle}</i>
+		</div>
+
+		<style jsx>{`
+			.game {
+				margin-bottom: 1.2em;
+				font-size: 0.9em;
+			}
+			a {
+				font-size: 1.3em;
+			}
+		`}</style>
+	</div>
+);
 
 export default MoreGames;
