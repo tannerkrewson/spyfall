1136c66d6aedd9b52014b60a216c536039145e97
Tanner Krewson <tannerkrewson@gmail.com>
2020-04-24 13:14:11 -0500

    better lock message

--- utils/misc.js
+++ utils/misc.js
@@ -1,6 +1,17 @@
-export const lockedMessage = (minutes) =>
-	"The Spyfall server is pending an update, and will be restarted in " +
-	minutes +
-	" minute" +
-	(parseInt(minutes) !== 1 ? "s" : "") +
-	". Try again then!";
+export const lockedMessage = (minutes) => ({
+	icon: "error",
+	title: "Oopsie woopsie",
+	text:
+		"The Spyfall server is pending an update, and will be restarted " +
+		getTimeLeft(minutes) +
+		". Try again then!",
+	footer:
+		"If you're the techy type, check the update status " +
+		'<a href="https://github.com/tannerkrewson/spyfall/actions" ' +
+		'target="_blank" rel="noopener noreferrer">here</a>.',
+});
+
+const getTimeLeft = (minutes) => {
+	if (minutes <= 0) return "momentarily";
+	return "in " + minutes + " minute" + (parseInt(minutes) !== 1 ? "s" : "");
+};
