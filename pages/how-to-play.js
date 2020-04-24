2f3272f2e04f0ffa8276fa3126eb88be2e6cfb98
Tanner Krewson <tannerkrewson@gmail.com>
2020-04-24 12:08:07 -0500

    added summary to how to play

--- pages/how-to-play.js
+++ pages/how-to-play.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Link from "next/link";
 
-import Page from "../components/Page";
-
 const HowToPlay = () => {
 	return (
 		<>
@@ -11,8 +9,11 @@ const HowToPlay = () => {
 				<hr />
 				<div>
 					<p>
-						Spyfall is based off the board game of the same name, so if you've
-						played that before, you'll understand Spyfall.
+						Spyfall is based off of the board game of the same name, so if
+						you've played that before, you'll understand the web version. You'll
+						need at least 3 or 4 players, but Spyfall is best with 5 or 6
+						players. You can also have 7 or more players, but the game may
+						become too easy for the spy.
 					</p>
 					<p>
 						In my opinion, the easiest way to learn how to play Spyfall would be
@@ -27,6 +28,22 @@ const HowToPlay = () => {
 						/>
 					</div>
 					<br />
+					<p>
+						In short, when the game begins, one random player will become the
+						spy, and all others will be given a location, and a role within the
+						location. For example, if the location of a round was "restaurant,"
+						one player might be the chef, another the waiter, another the
+						customer, etc. The players will not know who the spy is, and the spy
+						will not know the location. Players take turns asking questions to
+						each other, doing their best not to outright reveal the location in
+						their questions and answers, but not being too vague as to raise
+						suspicion. The non-spy group of players wins if they unanimously
+						agree on the identity of the spy player. The spy wins if they figure
+						out the location, which they have one chance to yell out at any time
+						during the round. The spy also wins if the other players unanimously
+						accuse someone else, or cannot unanimously decide on someone to
+						accuse.
+					</p>
 					<p>
 						If you want a little more detail, you can read the full official
 						rulebook of the board game version of Spyfall{" "}
